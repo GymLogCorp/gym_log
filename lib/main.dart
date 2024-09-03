163fe4cc5ebb85463c7fc77ad9cad62f9f3a2cb6
4ntonio19 <vgoncalvescarvalho@outlook.com.br>
2024-09-03 19:10:07 -0300

    add important files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GymLog',
+      home: Scaffold(
+        backgroundColor: Color(0xFF1E1E1E),
+        body: Center(
+          child: Container(
+            child: Image.asset('./assets/images/logoSemFundo.jpeg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
