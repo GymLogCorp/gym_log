122b3cae1b755e7f3bd4a0fa74230ddc82450066
Arthur Santos Oliveria <pessoalarthursoliveira@gmail.com>
2024-09-12 23:30:11 -0300

    trabalho honesto
    ainda faltam ajeitar alguns detalhes com relação a cores de texto

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -4,28 +4,64 @@ class InputField extends StatelessWidget {
   final String labelText;
   final bool obscureText;
   final TextEditingController? controller;
+  final String? title;
+  final Color titleColor;
+  final Color borderColor;
+  final Color fillColor;
 
   const InputField({
-    Key? key,
+    super.key,
     required this.labelText,
     this.obscureText = false,
     this.controller,
-  }) : super(key: key);
+    this.title,
+    this.titleColor = Colors.white,
+    this.borderColor = Colors.grey,
+    this.fillColor = Colors.white,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 300, // Defina a largura desejada aqui
-        child: TextField(
-          controller: controller,
-          obscureText: obscureText,
-          decoration: InputDecoration(
-            filled: true,
-            labelText: labelText,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
+    return SizedBox(
+      width: 320,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                title!,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: titleColor,
+                    ),
+              ),
+            ),
+          TextField(
+            obscureText: obscureText,
+            controller: controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: fillColor,
+              labelText: labelText,
+              labelStyle: TextStyle(color: borderColor),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(color: borderColor),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(color: borderColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(color: borderColor),
+              ),
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gym_log/components/input.dart';
+import 'package:gym_log/components/button.dart';
+import 'package:gym_log/pages/login.dart';
 
 class Register extends StatelessWidget {
   const Register({super.key});
+  void navigateToLogin(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Login(),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +22,35 @@ class Register extends StatelessWidget {
           alignment: Alignment.center,
           child: Padding(
             padding: const EdgeInsets.only(top: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: ListView(
               children: [
                 Image.asset('assets/images/logo_branca_sem_fundo.png'),
                 Column(
-                  children: [InputField(labelText: 'Insira seu nome')],
+                  children: [
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const InputField(
+                        labelText: 'Insira seu nome completo',
+                        title: 'Nome Copmleto'),
+                    const InputField(
+                        labelText: 'Insira seu email', title: 'Email'),
+                    const InputField(
+                        labelText: 'Insira sua senha', title: 'Senha'),
+                    const InputField(
+                        labelText: 'Inseria sua senha',
+                        title: 'Confirmar senha'),
+                    const SizedBox(height: 25.0),
+                    Button(
+                      label: 'Cadastrar',
+                      bgColor: 0xFF617AFA,
+                      textColor: 0xFFFFFFFF,
+                      borderColor: 0xFF617AFA,
+                      onPressedProps: () {
+                        navigateToLogin(context);
+                      },
+                    ),
+                  ],
                 )
               ],
             ),
