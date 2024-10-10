203c9f450d3b07e99fbaf06b4d1d99eacbf2c299
RenanBatista97 <rcbatista0@gmail.com>
2024-10-09 23:41:28 -0300

    adição da lista e botão para adicionar treino

--- lib/pages/workout/workout.dart
+++ lib/pages/workout/workout.dart
@@ -24,41 +24,41 @@ class SimplifiedWorkoutModel {
 
 class _WorkoutPageState extends State<WorkoutPage> {
   final List<SimplifiedWorkoutModel> simplifiedWorkoutList = [
-    // SimplifiedWorkoutModel(
-    //   id: 1,
-    //   name: 'PushDay',
-    //   muscleGroup: 'Peito',
-    //   exercisesCount: 6,
-    // ),
-    // SimplifiedWorkoutModel(
-    //   id: 2,
-    //   name: 'Costas e Bíceps',
-    //   muscleGroup: 'Costas',
-    //   exercisesCount: 6,
-    // ),
-    // SimplifiedWorkoutModel(
-    //   id: 3,
-    //   name: 'Espanca Perna',
-    //   muscleGroup: 'Quadriceps',
-    //   exercisesCount: 11,
-    // ),
-    // SimplifiedWorkoutModel(
-    //   id: 4,
-    //   name: 'aaaaaa',
-    //   muscleGroup: 'Posterior',
-    //   exercisesCount: 11,
-    // ),
-    // SimplifiedWorkoutModel(
-    //     id: 5,
-    //     name: 'mau mau homofobico',
-    //     muscleGroup: 'Antebraco',
-    //     exercisesCount: 4),
-    // SimplifiedWorkoutModel(
-    //   id: 6,
-    //   name: 'Ombreta cebolão',
-    //   muscleGroup: 'Ombro',
-    //   exercisesCount: 4,
-    // ),
+    SimplifiedWorkoutModel(
+      id: 1,
+      name: 'PushDay',
+      muscleGroup: 'Peito',
+      exercisesCount: 6,
+    ),
+    SimplifiedWorkoutModel(
+      id: 2,
+      name: 'Costas e Bíceps',
+      muscleGroup: 'Costas',
+      exercisesCount: 6,
+    ),
+    SimplifiedWorkoutModel(
+      id: 3,
+      name: 'Espanca Perna',
+      muscleGroup: 'Quadriceps',
+      exercisesCount: 11,
+    ),
+    SimplifiedWorkoutModel(
+      id: 4,
+      name: 'aaaaaa',
+      muscleGroup: 'Posterior',
+      exercisesCount: 11,
+    ),
+    SimplifiedWorkoutModel(
+        id: 5,
+        name: 'mau mau homofobico',
+        muscleGroup: 'Antebraco',
+        exercisesCount: 4),
+    SimplifiedWorkoutModel(
+      id: 6,
+      name: 'Ombreta cebolão',
+      muscleGroup: 'Ombro',
+      exercisesCount: 4,
+    ),
   ];
   @override
   Widget build(BuildContext context) {
@@ -90,12 +90,16 @@ class _WorkoutPageState extends State<WorkoutPage> {
                 itemCount: simplifiedWorkoutList.length,
                 itemBuilder: (context, index) {
                   final workout = simplifiedWorkoutList[index];
-                  return Text(
-                    workout.name,
-                    style: TextStyle(color: Colors.white),
-                  );
+                  return cardWorkout(workout);
                 },
-              )
+              ),
+        Button(
+          onPressedProps: () {},
+          label: 'Adicionar  Treino',
+          bgColor: 0xFF617AFA,
+          textColor: 0xFFFFFFFF,
+          borderColor: 0xFF617AFA,
+        ),
       ],
     );
   }
@@ -123,6 +127,69 @@ class _WorkoutPageState extends State<WorkoutPage> {
           borderColor: 0xFF617AFA,
         )
       ],
+    );
+  }
+
+  Widget cardWorkout(SimplifiedWorkoutModel workout) {
+    return Padding(
+      padding:
+          const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 5.0, top: 5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Wrap(
+            children: [
+              Container(
+                padding: EdgeInsets.all(8), // Espaçamento interno para o ícone
+                decoration: BoxDecoration(
+                  color: Color(0xFFE40928), // Cor de fundo da moldura
+                  borderRadius: BorderRadius.circular(7), // Bordas arredondadas
+                ),
+                child: Icon(
+                  Icons.fitness_center, // Ícone
+                  color: Colors.white, // Cor do ícone
+                  size: 20, // Tamanho do ícone
+                ),
+              ),
+              SizedBox(width: 8.0),
+              // Coluna com título e subtítulo
+              Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Alinha à esquerda
+                children: [
+                  Container(
+                    child: Text(
+                      workout.name,
+                      style: GoogleFonts.plusJakartaSans(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 1), // Espaçamento entre título e subtítulo
+                  Text(
+                    workout.muscleGroup,
+                    style: GoogleFonts.plusJakartaSans(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(
+            workout.exercisesCount.toString(), // Numeração
+            style: GoogleFonts.plusJakartaSans(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
