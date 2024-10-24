6827748102f3f9379034bccbf1e6c5755d8d250a
Maurício Moura da Veiga <mauricioveiga27@gmail.com>
2024-10-23 23:17:19 -0300

    componente card das series finalizado com seus textos, inputs e icones ao lado dele

--- lib/pages/session/seriescard.dart
+++ lib/pages/session/seriescard.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gym_log/models/exercise.dart';
 import 'package:sizer/sizer.dart';
@@ -17,7 +19,6 @@ class _CardSeriesState extends State<CardSeries> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const SizedBox(height: 5.0),
         SizedBox(
           width: 100.0.w,
           height: 6.0.h,
@@ -27,9 +28,7 @@ class _CardSeriesState extends State<CardSeries> {
                 side: const BorderSide(color: Colors.white, width: 1.0)),
             color: const Color(0xFF212429),
             child: Padding(
-              padding: EdgeInsets.only(
-                left: 20.0,
-              ),
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -41,12 +40,12 @@ class _CardSeriesState extends State<CardSeries> {
                       fontSize: 18.sp,
                     ),
                   ),
-                  IconButton(
-                    onPressed: () => {},
-                    icon: const Icon(
+                  InkWell(
+                    onTap: () => {},
+                    child: const Icon(
                       Icons.add_circle,
                       color: Color(0xFF617AFA),
-                      size: 32,
+                      size: 26,
                     ),
                   ),
                 ],
@@ -55,111 +54,127 @@ class _CardSeriesState extends State<CardSeries> {
           ),
         ),
         SizedBox(
-          width: 80.0.w,
-          height: 10.0.h,
-          child: Column(
+          width: 72.0.w,
+          height: 4.0.h,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      'Séries',
-                      style: GoogleFonts.plusJakartaSans(
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white,
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    SizedBox(width: 0.sp),
-                    Text(
-                      'Histórico',
-                      style: GoogleFonts.plusJakartaSans(
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white,
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    SizedBox(width: 0.sp),
-                    Text(
-                      'Kg',
-                      style: GoogleFonts.plusJakartaSans(
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white,
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    SizedBox(width: 0.sp),
-                    Text(
-                      'Repetições',
-                      style: GoogleFonts.plusJakartaSans(
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white,
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    SizedBox(width: 15.sp),
-                  ],
+              Text(
+                'Séries',
+                style: GoogleFonts.plusJakartaSans(
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white,
+                  fontSize: 16.sp,
                 ),
               ),
-              const SizedBox(height: 5.0),
-              Padding(
-                padding: EdgeInsets.only(left: 5.0),
-                child: SizedBox(
-                  width: 100.0.w,
-                  height: 6.0.h,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        side:
-                            const BorderSide(color: Colors.white, width: 1.0)),
-                    color: const Color(0xFF1C1C21),
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                        left: 18.sp,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '1',
+              Text(
+                'Histórico',
+                style: GoogleFonts.plusJakartaSans(
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                ),
+              ),
+              Text(
+                'Kg',
+                style: GoogleFonts.plusJakartaSans(
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                ),
+              ),
+              Text(
+                'Repetições',
+                style: GoogleFonts.plusJakartaSans(
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 100.0.w,
+          height: 5.0.h,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: const BorderSide(color: Colors.white, width: 1.0),
+                  ),
+                  color: const Color(0xFF1C1C21),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.sp, vertical: 8.sp),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '1',
+                          style: GoogleFonts.plusJakartaSans(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 18.sp,
+                          ),
+                        ),
+                        SizedBox(width: 1.sp),
+                        Text(
+                          '20x10',
+                          style: GoogleFonts.plusJakartaSans(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 18.sp,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 27.sp,
+                          height: 20.sp,
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
                             style: GoogleFonts.plusJakartaSans(
                               fontWeight: FontWeight.bold,
                               color: Colors.white,
                               fontSize: 18.sp,
                             ),
                           ),
-                          Text(
-                            '20x10',
+                        ),
+                        SizedBox(
+                          width: 27.sp,
+                          height: 20.sp,
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
                             style: GoogleFonts.plusJakartaSans(
                               fontWeight: FontWeight.bold,
                               color: Colors.white,
                               fontSize: 18.sp,
                             ),
                           ),
-                          Text(
-                            '22',
-                            style: GoogleFonts.plusJakartaSans(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 18.sp,
-                            ),
-                          ),
-                          SizedBox(width: 0.sp),
-                          Text(
-                            '12',
-                            style: GoogleFonts.plusJakartaSans(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 18.sp,
-                            ),
-                          ),
-                          SizedBox(width: 25.sp),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
+                ),
+              ),
+              SizedBox(width: 3.0.w),
+              InkWell(
+                onTap: () => {},
+                child: const Icon(
+                  Icons.check_circle_outline_outlined,
+                  color: Color(0xFF617AFA),
+                  size: 32,
+                ),
+              ),
+              SizedBox(width: 2.0.w),
+              InkWell(
+                onTap: () => {},
+                child: const Icon(
+                  Icons.delete_forever_rounded,
+                  color: Color(0xFFE40928),
+                  size: 32,
                 ),
               ),
             ],
