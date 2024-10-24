78e7abe7f836b13eca172fe99f2e61b3a18a98bd
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2024-10-24 20:09:20 -0300

    séries de acordo com qtd
    quebre oque conseguir

--- lib/pages/session/seriescard.dart
+++ lib/pages/session/seriescard.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:gym_log/models/exercise.dart';
 import 'package:sizer/sizer.dart';
@@ -24,11 +23,12 @@ class _CardSeriesState extends State<CardSeries> {
           height: 6.0.h,
           child: Card(
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                side: const BorderSide(color: Colors.white, width: 1.0)),
+              borderRadius: BorderRadius.circular(10.0),
+              side: const BorderSide(color: Colors.white, width: 1.0),
+            ),
             color: const Color(0xFF212429),
             child: Padding(
-              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -94,91 +94,97 @@ class _CardSeriesState extends State<CardSeries> {
             ],
           ),
         ),
-        SizedBox(
-          width: 100.0.w,
-          height: 5.0.h,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    side: const BorderSide(color: Colors.white, width: 1.0),
-                  ),
-                  color: const Color(0xFF1C1C21),
-                  child: Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.sp, vertical: 8.sp),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '1',
-                          style: GoogleFonts.plusJakartaSans(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 18.sp,
-                          ),
+        Column(
+          children: List.generate(widget.exercise.countSeries, (index) {//aqui que a mágica acontece
+            return SizedBox(
+              width: 100.0.w,
+              height: 5.0.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: const BorderSide(color: Colors.white, width: 1.0),
+                      ),
+                      color: const Color(0xFF1C1C21),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20.sp,
+                          vertical: 8.sp,
                         ),
-                        SizedBox(width: 1.sp),
-                        Text(
-                          '20x10',
-                          style: GoogleFonts.plusJakartaSans(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 18.sp,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 27.sp,
-                          height: 20.sp,
-                          child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            style: GoogleFonts.plusJakartaSans(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 18.sp,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '${index + 1}', //se não entender isso aqui tranca o curso
+                              style: GoogleFonts.plusJakartaSans(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18.sp,
+                              ),
                             ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 27.sp,
-                          height: 20.sp,
-                          child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            style: GoogleFonts.plusJakartaSans(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 18.sp,
+                            SizedBox(width: 1.sp),
+                            Text(
+                              '${widget.exercise.countSeries}x${widget.exercise.countRepetition}', //"""histórico"""
+                              style: GoogleFonts.plusJakartaSans(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 18.sp,
+                              ),
                             ),
-                          ),
+                            SizedBox(
+                              width: 27.sp,
+                              height: 20.sp,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                style: GoogleFonts.plusJakartaSans(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 18.sp,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 27.sp,
+                              height: 20.sp,
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                style: GoogleFonts.plusJakartaSans(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 18.sp,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(width: 3.0.w),
+                  InkWell(
+                    onTap: () => {},
+                    child: const Icon(
+                      Icons.check_circle_outline_outlined,
+                      color: Color(0xFF617AFA),
+                      size: 32,
+                    ),
+                  ),
+                  SizedBox(width: 2.0.w),
+                  InkWell(
+                    onTap: () => {},
+                    child: const Icon(
+                      Icons.delete_forever_rounded,
+                      color: Color(0xFFE40928),
+                      size: 32,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: 3.0.w),
-              InkWell(
-                onTap: () => {},
-                child: const Icon(
-                  Icons.check_circle_outline_outlined,
-                  color: Color(0xFF617AFA),
-                  size: 32,
-                ),
-              ),
-              SizedBox(width: 2.0.w),
-              InkWell(
-                onTap: () => {},
-                child: const Icon(
-                  Icons.delete_forever_rounded,
-                  color: Color(0xFFE40928),
-                  size: 32,
-                ),
-              ),
-            ],
-          ),
+            );
+          }),
         ),
       ],
     );
